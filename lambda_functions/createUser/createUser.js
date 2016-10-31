d1d78dd5d5101952399a84f42a75bd4678f0ed97
Patrick Brandt <pbrandt@coca-cola.com>
2016-10-31 17:04:01 -0400

    WIP: init commit createUser

---
+++ lambda_functions/createUser/createUser.js
@@ -0,0 +1,10 @@
+'use strict';
+var log = require('../../lib/log');
+
+module.exports = function(event, context, cb) {
+    //TODO: get username and password from response, has password and store in database
+    //NOTE: need to authorize client Id
+
+    
+
+}
---
+++ lambda_functions/createUser/createUser.test.js
@@ -0,0 +1,9 @@
+'use strict';
+var proxyquire = require('proxyquire'),
+    chai = require('chai'),
+    sinon = require('sinon'),
+    assert = chai.assert,
+    expect = chai.expect,
+    testHelper = require('../../util/testHelper'),
+    constants = require('../../lib/constants');
+
