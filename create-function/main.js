b66ce4e12d7386a1060624037864ab2c59cbef0c
Aaron Bawcom <abawcom@coca-cola.com>
2016-11-06 00:28:14 -0400

    Taking a stab at custom serverless plugin to create func if it doens't exist

---
+++ create-function/main.js
@@ -0,0 +1,56 @@
+'use strict';
+
+class ProviderDeployFunction {
+  constructor(serverless, options) {
+    // console.log( 'Here', serverless );
+    this.serverless = serverless;
+    this.options = options;
+    this.aws = this.serverless.getProvider('aws');
+    this.function = serverless.service.functions[ this.options.f ];
+    // console.log( this.options, this.function );
+    this.runtime = this.serverless.service.provider.runtime;
+
+    // set the providers name here
+    this.provider = 'aws';
+
+    this.commands = {
+      deploy: {
+        commands: {
+          function: {
+            lifecycleEvents: [
+              'deploy'
+            ]
+          },
+        }        
+      },
+    };
+    this.hooks = {
+      'before:deploy:function:deploy': this.beforeDeployFunction.bind(this)
+    };
+
+  }
+
+  beforeDeployFunction() {
+    
+    let params = {
+      FunctionName: this.options.functionObj.name,
+    };
+
+    this.provider.request( 'Lambda', 'getFunction', params, this.options.stage, this.serverless.service.provider.region )
+    .catch( () => {
+      params = {
+        Code: {
+          ZipFile: this.function.handler + ' = function() {}'
+        },
+        FunctionName: this.options.function,
+        Handler: this.function.handler,
+        Role: this.serverless.service.custom.IamRoleArnForCreate,
+        Runtime: this.runtime
+      };
+      return this.aws.request('Lambda', 'createFunction', params, this.options.stage, this.serverless.service.provider.region)
+      .then( (output) => console.log );
+    });
+  }
+}
+
+module.exports = ProviderDeployFunction;
