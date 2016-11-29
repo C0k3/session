53e04f07851333afa6eef0e1c4910993c30cd2d4
Patrick Brandt <pbrandt@coca-cola.com>
2016-11-29 12:37:06 -0500

    converting configs to javascript files

---
+++ config/patrick.js
@@ -0,0 +1,17 @@
+module.exports = {
+  RefreshTokenTable: 'patrick-KODemoTokens',
+  UsersTable: 'patrick-KODemoUsers',
+  RefreshTokenExpiration: '30 days',
+  AccessTokenExpiration: '1h',
+  LogLevel: 'debug',
+  DynamoDB: {
+    access_key: 'test',
+    secret_key: 'test',
+    region: 'us-east-1',
+    endpoint: 'https://dynamodb.us-east-1.amazonaws.com'
+  },
+//NOTE: DO NOT STORE SALT IN CODE --> this is for demo puposes only
+//in production consider an encrypted value in code and store the decryption key in AWS KMS
+  apiIdSalt: '{apiId_salt}',
+  passwordSalt: '{password_salt}'
+}
